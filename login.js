fc189769da11063599045e0310c64474b99e9bfa
ARPAN DHARA <arpandhara294@gmail.com>
2025-10-23 15:47:13 +0530

    few changes

--- login.js
+++ login.js
@@ -3,5 +3,5 @@ submitBtn = document.querySelector(".forms_submit");
 submitBtn.addEventListener('click' , (e)=>{
     e.preventDefault();
 
-    window.location.href = '../chat_interface/chat_interface.html'
+    window.location.href = 'chat_interface/chat_interface.html'
 })
